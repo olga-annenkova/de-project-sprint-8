5c70f700324c9aca9b82154852a2c0ff2f3b2675
Ola Annenkova <ola@MacBook-Pro-Alexander.local>
2025-09-19 13:48:49 +0300

    feat: spirnt 8 project first commit

---
+++ src/table_for_de8.sql
@@ -0,0 +1,24 @@
+-- Входная таблица
+CREATE TABLE public.subscribers_restaurants (
+    id serial4 NOT NULL,
+    client_id varchar NOT NULL,s
+    restaurant_id varchar NOT NULL,
+    CONSTRAINT pk_id PRIMARY KEY (id)
+);
+
+-- Выходная таблица
+CREATE TABLE public.subscribers_feedback (
+    id serial4 NOT NULL,
+    restaurant_id text NOT NULL,
+    adv_campaign_id text NOT NULL,
+    adv_campaign_content text NOT NULL,
+    adv_campaign_owner text NOT NULL,
+    adv_campaign_owner_contact text NOT NULL,
+    adv_campaign_datetime_start int8 NOT NULL,
+    adv_campaign_datetime_end int8 NOT NULL,
+    datetime_created int8 NOT NULL,
+    client_id text NOT NULL,
+    trigger_datetime_created int4 NOT NULL,
+    feedback varchar NULL,
+    CONSTRAINT id_pk PRIMARY KEY (id)
+);
